e5fd7776ce811687b34ff7a40bd9716a3f003906
Ben-Bingham <ben@benbingham.ca>
2022-06-01 17:54:23 -0600

    Basic screen with variable size implemented

--- ASCII Graphing Calculator/src/Main.cpp
+++ ASCII Graphing Calculator/src/Main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <string>
+
+#define SCREEN "."
+#define LINE "*"
+
+int screenWidth = 100;
+int screenHeight = 20;
 
 int main() {
-	std::cout << "Hello World" << std::endl;
+
+	std::string screen;
+
+	for (int i = 0; i < screenHeight; i++) {
+		for (int j = 0; j < screenWidth; j++) {
+			screen += SCREEN;
+		}
+		screen += "\n";
+	}
+
+	std::cout << screen << std::endl;
 
 	std::cin.get();
 	return 0;
