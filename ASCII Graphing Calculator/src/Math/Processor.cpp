8496c768c1a7d1466e63707cfa2c51f91e690c52
Ben-Bingham <ben@benbingham.ca>
2022-06-04 17:09:50 -0600

    Basic Processor implemented

--- ASCII Graphing Calculator/src/Math/Processor.cpp
+++ ASCII Graphing Calculator/src/Math/Processor.cpp
@@ -23,4 +23,39 @@ namespace Math {
 		}
 	}
 
+	double Processor::evaluateVariableExpression(VariableExpression expression, double variableValue) {
+		double val = expression.getValue();
+		if (expression.getVarBeforeVal()) {
+			switch (expression.getOperator()) {
+			case Additon:
+				return variableValue + val;
+			case Subtraction:
+				return variableValue - val;
+			case Multiplication:
+				return variableValue * val;
+			case Division:
+				return variableValue / val;
+			case Power:
+				return std::pow(variableValue, val);
+			default:
+				return 0;
+			}
+		}
+		else {
+			switch (expression.getOperator()) {
+			case Additon:
+				return val + variableValue;
+			case Subtraction:
+				return val - variableValue;
+			case Multiplication:
+				return val * variableValue;
+			case Division:
+				return val / variableValue;
+			case Power:
+				return std::pow(val, variableValue);
+			default:
+				return 0;
+			}
+		}
+	}
 }
